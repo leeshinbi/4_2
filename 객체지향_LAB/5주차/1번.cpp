e52c8e82373748128c110cb303f168a727932b0f
01_49g_63m <tlsql6831@naver.com>
2023-10-11 23:45:37 +0900

    Update 1번.cpp

--- 객체지향_LAB/5주차/1번.cpp
+++ 객체지향_LAB/5주차/1번.cpp
@@ -2,37 +2,38 @@
 #include <vector>
 #include <string>
 #include <algorithm> // STL 알고리즘을 사용하기 위해 필요한 헤더 파일
+using namespace std;
 
 int main() {
     // a번
-    std::vector<std::string> vec;   
+    vector<string> vec;
 
     // b번
     vec.push_back("MILK");
     vec.push_back("BUTTER");
     vec.push_back("BREAD");
     vec.push_back("SALAD");
-    vec.push_back("CUCUMBER");   
+    vec.push_back("CUCUMBER");
 
     // c번
-    for (const std::string& item : vec) {
-        std::cout << item << std::endl;
-    } 
+    for (const string& item : vec) {
+        cout << item << endl;
+    }
 
     // d번 
-    std::vector<std::string>::iterator p;
+    vector<string>::iterator p;
 
     // e번 
     for (p = vec.begin(); p != vec.end(); ++p) {
-        std::cout << *p << std::endl;
+        cout << *p << endl;
     }
 
     // f번
-    std::sort(vec.begin(), vec.end());
+    sort(vec.begin(), vec.end());
 
     // g번 
     for (p = vec.begin(); p != vec.end(); ++p) {
-        std::cout << *p << std::endl;
+        cout << *p << endl;
     }
 
     return 0;
